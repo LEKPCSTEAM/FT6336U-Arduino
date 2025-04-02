4f73653c1bf3e0cda1157a15e2b7537eec4d32df
lek <alex2530lu@gmail.com>
2025-04-02 15:13:26 +0700

    first commit

---
+++ src/FT6336U.cpp
@@ -0,0 +1,53 @@
+#include "FT6336U.h"
+
+FT6336U::FT6336U(uint8_t sda_pin, uint8_t scl_pin) {
+    _sdaPin = sda_pin;
+    _sclPin = scl_pin;
+}
+bool FT6336U::begin() {
+    Wire.begin(_sdaPin, _sclPin, (uint32_t) 400E3);
+    Wire.beginTransmission(FT6336U_I2C_ADDR);
+    return (Wire.endTransmission() == 0);
+}
+
+uint8_t FT6336U::getNumTouches() {
+    Wire.beginTransmission(FT6336U_I2C_ADDR);
+    Wire.write(FT6336U_REG_NUM_TOUCHES);
+    Wire.endTransmission();
+    Wire.requestFrom(FT6336U_I2C_ADDR, 1);
+    return Wire.read();
+}
+
+bool FT6336U::getTouchCoordinates(uint16_t &x, uint16_t &y) {
+    Wire.beginTransmission(FT6336U_I2C_ADDR);
+    Wire.write(FT6336U_REG_TOUCH1_XH);
+    Wire.endTransmission();
+    Wire.requestFrom(FT6336U_I2C_ADDR, 4);
+    
+    if (Wire.available() == 4) {
+        x = (Wire.read() & 0x0F) << 8;
+        x |= Wire.read();
+        y = (Wire.read() & 0x0F) << 8;
+        y |= Wire.read();
+        return true;
+    }
+    return false;
+}
+
+void FT6336U::calibrateScreen() {
+    Serial.println("Touch the top-left corner");
+    while (getNumTouches() == 0);
+    while (getNumTouches() > 0) {
+        getTouchCoordinates(rawTouchX[0], rawTouchY[0]);
+    }
+    delay(500);
+
+    Serial.println("Touch the bottom-right corner");
+    while (getNumTouches() == 0);
+    while (getNumTouches() > 0) {
+        getTouchCoordinates(rawTouchX[1], rawTouchY[1]);
+    }
+    delay(500);
+
+    Serial.println("Calibration completed!");
+}
---
+++ src/FT6336U.h
@@ -0,0 +1,33 @@
+
+#ifndef FT6336U_H
+#define FT6336U_H
+
+#include <Arduino.h>
+#include <Wire.h>
+
+/* สำหรับจอที่ใช้ชิปทัช FT6336U */
+class FT6336U
+{
+public:
+    FT6336U(uint8_t sda_pin = TOUCH_SDA_PIN, uint8_t scl_pin = TOUCH_SCL_PIN);
+    bool begin();
+    uint8_t getNumTouches();
+    bool getTouchCoordinates(uint16_t &x, uint16_t &y);
+    void calibrateScreen();
+
+    // Calibration data
+    uint16_t calX[2] = {0, 479};
+    uint16_t calY[2] = {0, 319};
+    uint16_t rawTouchX[2] = {0, 0};
+    uint16_t rawTouchY[2] = {0, 0};
+
+private:
+    static const uint8_t FT6336U_I2C_ADDR = 0x38;
+    static const uint8_t FT6336U_REG_NUM_TOUCHES = 0x02;
+    static const uint8_t FT6336U_REG_TOUCH1_XH = 0x03;
+    static const uint8_t FT6336U_REG_TOUCH1_XL = 0x04;
+    static const uint8_t FT6336U_REG_TOUCH1_YH = 0x05;
+    static const uint8_t FT6336U_REG_TOUCH1_YL = 0x06;
+};
+
+#endif
